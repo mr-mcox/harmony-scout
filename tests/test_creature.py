03ad6fa262c500d61ddff811437dac15f864cbea
Matthew Cox <mr.mcox@gmail.com>
2018-06-26 20:37:18 -0500

    Black formatting

--- tests/test_creature.py
+++ tests/test_creature.py
@@ -3,14 +3,17 @@ import pytest
 import numpy as np
 from numpy.testing import assert_almost_equal
 
-@pytest.mark.parametrize('before,after',
-                         [
-                             ([0.1, 0.2, 0.3], [0.1, 0.2, 0.3]),
-                             ([0.2, 0.1, 0.3], [0.1, 0.2, 0.3]),
-                             ([0.1, 0.2, -0.7], [0.1, 0.2, 0.3]),
-                             ([0.1, 0.2, 0.8], [-0.2, 0.1, 0.2]),
-                             ([0.4, 0.8, 0.8], [-0.2, -0.2, 0.4]),
-                         ])
+
+@pytest.mark.parametrize(
+    "before,after",
+    [
+        ([0.1, 0.2, 0.3], [0.1, 0.2, 0.3]),
+        ([0.2, 0.1, 0.3], [0.1, 0.2, 0.3]),
+        ([0.1, 0.2, -0.7], [0.1, 0.2, 0.3]),
+        ([0.1, 0.2, 0.8], [-0.2, 0.1, 0.2]),
+        ([0.4, 0.8, 0.8], [-0.2, -0.2, 0.4]),
+    ],
+)
 def test_conform_genotype(before, after):
     c = PitchClassCreature()
     gene = np.array([before])
